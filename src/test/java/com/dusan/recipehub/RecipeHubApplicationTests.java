3a2349b335898d139918e17731dfde8f848f7631
Dusan <dusan.stojiljkovic.5835@metropolitan.ac.rs>
2025-07-06 19:15:06 +0200

    Initial commit

---
+++ src/test/java/com/dusan/recipehub/RecipeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dusan.recipehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
